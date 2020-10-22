4a71fb5ed71f4fa3be8984d4c63cb10d3de24fd1
nahid <asdasd>
2020-10-22 17:08:53 -0400

    added projects and better commenting out

--- DigitalOcean/main.tf
+++ DigitalOcean/main.tf
@@ -1,5 +1,6 @@
 # Mail server for https://github.com/n0pe-sled/Postfix-Server-Setup
 resource "digitalocean_droplet" "mail" {
+  #project = "Something"            #Project bucket name, if you have something setup
   image = "ubuntu-16-04-x64"        #ubuntu-18-04-x64 ubuntu-20-04-x64
   name = "mail"
   region = "nyc1"                   #https://www.digitalocean.com/docs/networking/vpc/                                 
@@ -10,14 +11,14 @@ resource "digitalocean_droplet" "mail" {
 }
 
 # VPN box
-/* 
-resource "digitalocean_droplet" "vpn" {
-  image = "ubuntu-20-04-x64"        #ubuntu-18-04-x64 ubuntu-20-04-x64
-  name = "vpn"
-  region = "nyc1"                   #https://www.digitalocean.com/docs/networking/vpc/                                 
-  size = "s-1vcpu-1gb"              #https://developers.digitalocean.com/documentation/changelog/api-v2/new-size-slugs-for-droplet-plan-changes/
-  private_networking = true
-  ssh_keys = [""]                   #Can add multiple ssh key names here. Get from DigitalOcean
-  tags   = ["TEST1"]                #Add whatever tags you want
-} 
-*/
+#resource "digitalocean_droplet" "vpn" {
+#  #project = "Something"
+#  image = "ubuntu-20-04-x64"        #ubuntu-18-04-x64 ubuntu-20-04-x64
+#  name = "vpn"
+#  region = "nyc1"                   #https://www.digitalocean.com/docs/networking/vpc/                                 
+#  size = "s-1vcpu-1gb"              #https://developers.digitalocean.com/documentation/changelog/api-v2/new-size-slugs-for-droplet-plan-changes/
+#  private_networking = true
+#  ssh_keys = [""]                   #Can add multiple ssh key names here. Get from DigitalOcean
+#  tags   = ["TEST1"]                #Add whatever tags you want
+#} 
+
